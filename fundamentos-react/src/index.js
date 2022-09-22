ac247b107776e57d69ce0a338f66e793f033bcfa
DeniseBarbosa <deniseluvoir@gmail.com>
2022-09-22 15:28:30 -0300

    Com parametros

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,11 +3,15 @@ import ReactDom from 'react-dom' /**Aqui a variavel pode ser nomeada como quiser
 import React from 'react' /**importação obrigatoria a variavel não é nomeada  */
 
 import Primeiro from './componentes/basicos/Primeiro'
+import ComParametro from './componentes/basicos/ComParamentro'
 /**Observe que a palavra 'Primeiro' pode ser nomeada */
 ReactDom.render(
 
-    <div>
+    <div id='app'>
         <Primeiro></Primeiro>
+        <ComParametro 
+        titulo =" Aqui"
+        subtitulo = 'Subtitulo'/>
     </div>,
     document.getElementById('root')
 )
