fe15b7ae056e522e226696c1baab1b5218eeebba
DeniseBarbosa <deniseluvoir@gmail.com>
2022-09-22 09:38:13 -0300

    Referenciando um arquivo CSS a partir do código

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,10 +1,12 @@
-import ReactDom from 'react-dom'
-import React from 'react'
+import './index.css'
+import ReactDom from 'react-dom' /**Aqui a variavel pode ser nomeada como quiser */
+import React from 'react' /**importação obrigatoria a variavel não é nomeada  */
 
 const jsx = <strong> Outra forma de usar o JSX</strong>
 ReactDom.render(
+/** lembrando que jsx é apenas uma variavel nomeada  */
     <div>
-        {jsx} /** lembrando que jsx é apenas uma variavel nomeada  */
+        {jsx} 
     </div>,
     document.getElementById('root')
 )
