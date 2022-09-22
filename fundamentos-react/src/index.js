93129a65d124a66beeaf35bd96faa75b41702c78
DeniseBarbosa <deniseluvoir@gmail.com>
2022-09-22 09:24:22 -0300

    Outra forma de usar o JSX

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,10 +1,10 @@
 import ReactDom from 'react-dom'
 import React from 'react'
 
-
+const jsx = <strong> Outra forma de usar o JSX</strong>
 ReactDom.render(
     <div>
-        <strong> Usando o JSX</strong>
+        {jsx} /** lembrando que jsx é apenas uma variavel nomeada  */
     </div>,
     document.getElementById('root')
 )
