fd5093ef7474d7ec0a6031b633b7df2200cf9f4c
DeniseBarbosa <deniseluvoir@gmail.com>
2022-09-22 12:31:48 -0300

    Primeiro componente

---
+++ fundamentos-react/src/componentes/basicos/Primeiro.js
@@ -0,0 +1,7 @@
+/**A função pode ser anonima, ela pode ser escrita tambem dessa forma */
+/** export default function (){
+    return 
+}*/
+export default function Primeiro(){
+    return "Primeiro componentes"
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,12 @@ import './index.css'
 import ReactDom from 'react-dom' /**Aqui a variavel pode ser nomeada como quiser */
 import React from 'react' /**importação obrigatoria a variavel não é nomeada  */
 
-const jsx = <strong> Outra forma de usar o JSX</strong>
+import Primeiro from './componentes/basicos/Primeiro'
+/**Observe que a palavra 'Primeiro' pode ser nomeada */
 ReactDom.render(
-/** lembrando que jsx é apenas uma variavel nomeada  */
+
     <div>
-        {jsx} 
+        <Primeiro></Primeiro>
     </div>,
     document.getElementById('root')
 )
