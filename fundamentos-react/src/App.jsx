de0fc5f4b3b933e814470e88e34cfefb77cb662e
DeniseBarbosa <deniseluvoir@gmail.com>
2022-09-23 18:11:32 -0300

    Card organização

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,17 +5,33 @@ import Primeiro from './componentes/basicos/Primeiro'
 import ComParametro from './componentes/basicos/ComParamentro'
 import Fragmentos from './componentes/basicos/Fragmentos'
 import Aleatorio from './componentes/basicos/Aleatorio'
+import Card from './componentes/layout/Card'
 
 export default function App(props){
     return(
         <div id='app'>
             <h1>Fundamentos React</h1>
-            <Fragmentos></Fragmentos>
-            <ComParametro 
-                 titulo =" Aqui"
-                 subtitulo = 'Subtitulo'/>
-            <Primeiro></Primeiro>
-            <Aleatorio min={1} max={60}></Aleatorio>
+
+            <Card titulo="#4 - Desafiou Aleatório">
+                 <Aleatorio min={1} max={60}></Aleatorio>
+            </Card>
+
+            <Card titulo="#3 - Fragmento">
+                <Fragmentos></Fragmentos>
+            </Card>
+
+            <Card titulo="#2 - Com Parâmentro">
+                <ComParametro 
+                    titulo =" Aqui"
+                    subtitulo = 'Subtitulo'/>
+            </Card>
+           
+            <Card titulo="#1 - Primeiro Componente">
+                <Primeiro></Primeiro>
+            </Card>
+            
+           
+            
     </div>
     )
 }
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,18 +2,13 @@ import './index.css'
 import ReactDom from 'react-dom' /**Aqui a variavel pode ser nomeada como quiser */
 import React from 'react' /**importação obrigatoria a variavel não é nomeada  */
 
-import Primeiro from './componentes/basicos/Primeiro'
-import ComParametro from './componentes/basicos/ComParamentro'
-import Fragmentos from './componentes/basicos/Fragmentos'
-/**Observe que a palavra 'Primeiro' pode ser nomeada */
+
+
+import App from './App'
+
 ReactDom.render(
 
-    <div id='app'>
-        <Primeiro></Primeiro>
-        <ComParametro 
-        titulo =" Aqui"
-        subtitulo = 'Subtitulo'/>
-        <Fragmentos></Fragmentos>
-    </div>,
+    <App></App>
+   ,
     document.getElementById('root')
 )
